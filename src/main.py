7827adda22fae920776bf3ba38f20c12abec66fd
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2026-03-15 16:04:02 +0000

    Now we are able to connect to the db
    Added a bash script to gitignore

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import psycopg2
 from dotenv import load_dotenv
 import os 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 load_dotenv()
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 print(engine.url)
 with engine.connect() as connection:
-    result = connection.execute("Select version();")
+    result = connection.execute(text("Select version();"))
     print(result.fetchone())
