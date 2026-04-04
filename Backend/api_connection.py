ab2cd4e605c179120a0e940345586211e24f4bd6
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2026-04-04 15:11:23 +0000

    Change of plans:
    Working on TRI (Toxic Release Inventory) of datacenters throught the years.
    
    api_connection.py:
    created a url_filter() and get_data() functions

--- Backend/api_connection.py
+++ Backend/api_connection.py
@@ -1,53 +1,58 @@
 import requests 
 import json
-from warcio.archiveiterator import ArchiveIterator as ai
-import io
 from time import sleep
+from dotenv import load_dotenv
+import os
+load_dotenv(override=True)
 
-url = "https://index.commoncrawl.org/collinfo.json"
-headers = {"User-Agent": "Mozilla/5.0"}
 
-for attempt in range(5):
+def url_filter(program = "tri",table = 'tri_chem_activity/', range = '1:20/', format = 'json', column = "", join = "", sort = ""):
+    
+    """
+    Creates a URL for the EPA's DMP API based on the provided parameters.
+    Keyword arguments:
+    program -- **required** the EPA program to query (default "tri")
+    table -- **required** the specific table within the program to query (defauly "tri_chem_activity/")
+    range -- the range of records to retrieve, formatted as "start:end" (default "1:20/")
+    column -- the column to filter
+    join -- Combine the results of two tables format  [join_type]/[table]/[comparison]
+    sort -- sort results in the field format /sort/[column]:[direction]/. Direction can either be "asc|desc"
+    
+    **Important**
+    URL Format: 
+    1. https://data.epa.gov/dmapservice/[table]/[column][operator][value]/[join]/[first]:[last]/[sort]/[format]
+    if you get an status code of 400 or 500, check your URL format and paramters
+    
+    2. Don't forget to add / at the end of each parameters, otherwise you will not get an url:
+    
+    3. Go to this URL for more information
+    https://www.epa.gov/enviro/web-services
+    
+    """
+    
+    # Base Url for the EPA DMP API
+    base_url = 'https://data.epa.gov/dmapservice'
+    if table and range and program:
+        url = f"{base_url}/{program}.{table}{column}{join}{range}{sort}{format}"
+        return url 
+    else:
+        print("Error: Missing required parameters. Please provide program, table, and range.")
+        return None
+
+def get_data(base_url):
     try:
-        response = requests.get(url, headers=headers, timeout = 10)
-        response.raise_for_status()
-        catalog = response.json()
-        all_ids = [c['id'] for c in catalog]
-        print("Collection IDs:", all_ids)
-        break
-    except requests.exceptions.RequestException as e:
-        print(f"Attempts: {attempt + 1} failed {e}")
-        sleep(2)
+        response = requests.get(base_url)
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Error: Received status code {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Error has occurred: {e}")
+        return None 
 
-# endpoint = f"https://index.commoncrawl.org/{latest_index}-index"
-
-# params = {
-#     "url": 'amazon.com',
-#     "output": 'json',
-#     "limit" : 5
-# }
-
-# res = requests.get(endpoint, params=params)
-# print(res)
-
-# records = [json.loads(line) for line in res.text.strip().split('\n')]
-
-# s3_urls = f"https://data.commoncrawl.org/{records[0]['filename']}"
-# offset = int(records[0]['offset'])
-# length = int(records[0]['length'])
-
-# headers = {"Range": f"bytes={offset}-{offset+length-1}"}
-# warc_res = requests.get(s3_urls,headers=headers)
-
-# print(warc_res.status_code)
-
-# stream = io.BytesIO(warc_res.content)
-# encoding =  records[0]['encoding']
-# print(type(encoding))
-# print(f"Encoding: {encoding}, Type: {type(encoding)}")
-
-# for rec in ai(stream):
-#     if rec.rec_type == 'response':
-#         html = rec.content_stream().read().decode(encoding, errors='ignore')
-#         print(html[:500])
-        
+base_url = url_filter()
+print(base_url)
+data = get_data(base_url)
+print(json.dumps(data, indent=4))
