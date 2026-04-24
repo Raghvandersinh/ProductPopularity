e0299562ee8b7956dcf530197b072c0af7f9dae1
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2026-04-24 15:55:45 +0000

    Connected Alembic to the database and created a migration file, but nothing added for now

--- TRI_migration/env.py
+++ TRI_migration/env.py
@@ -4,11 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+from dotenv import load_dotenv
+import os
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+load_dotenv()
 
+config.set_main_option('sqlalchemy.url', os.getenv("DATABASE_URL"))
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
